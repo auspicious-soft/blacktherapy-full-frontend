079f460d64bca5a49f8c7d4e5ce6ec165009c05c
Auspicious <info@auspicioussoft.com>
2024-11-22 13:50:15 +0530

    Enhance chat page layout by adjusting height, adding border, and improving input styles

--- src/app/customer/appointments/chats/[id]/page.tsx
+++ src/app/customer/appointments/chats/[id]/page.tsx
@@ -28,7 +28,10 @@ const Page = () => {
       document.body.classList.remove("chat-open");
     };
   }, []);
-  
+
+  const onTyping = (e:any) => {
+    setInputMessage(e.target.value)
+  }
   const sendMessage = () => {
     if (inputMessage.trim() === "") return;
 
@@ -43,87 +46,85 @@ const Page = () => {
     ]);
 
     setInputMessage("");
-  };
+  }
 
   return (
     <div>
-       <h1 className="font-antic text-[#283C63] text-[30px] leading-[1.2em] mb-[25px] lg:text-[40px] lg:mb-[50px]">
+      <h1 className="font-antic text-[#283C63] text-[30px] leading-[1.2em] mb-[25px] lg:text-[40px] lg:mb-[50px]">
         Wellness Portal
       </h1>
-      <div className="flex rel relative overflow-hidden flex-col h-[calc(100vh-148px)] bg-white rounded-t-[20px]">
-      {/* Header */}
-      <div className="flex items-center bg-[#26395E] rounded-t-[20px] text-white p-4">
-        <div className="flex items-center space-x-3">
+      <div className="flex rel relative overflow-hidden flex-col h-[calc(100vh-168px)] bg-white border rounded-[20px]">
+        {/* Header */}
+        <div className="flex items-center bg-[#26395E] rounded-t-[20px] text-white p-4">
+          <div className="flex items-center space-x-3">
             <Image src={imgg} height={200} width={200}
-            alt="User Avatar" className="w-10 h-10 rounded-full"/>
+              alt="User Avatar" className="w-10 h-10 rounded-full" />
             <div>
-            <h2 className="text-lg font-semibold text-white">Alison Kennedy</h2>
-            <p className="text-sm text-white ">Active Now</p>
-          </div>
-        </div>
-        <div className="ml-auto">
-          <button className="text-white text-xl">⋮</button>
-        </div>
-      </div>
-
-      {/* Messages */}
-      <div className="flex-1 bg-white overflow-y-auto overflo-custom p-4">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`flex ${message.key === "left" ? "justify-start" : "justify-end"} mb-4`}
-          >
-            <div
-              className={`max-w-[70%]  rounded-[10px] ${
-                message.key === "left"
-                  ? ""
-                  : ""
-              }`}
-            >
-              <p className={`p-3 rounded-[10px] ${
-                message.key === "left"
-                  ? "bg-[#E7F8F6] shadow-md"
-                  : "bg-[#CCE9FA]  shadow-md"
-              }`}>{message.text}</p>
-
-              <p className={`text-xs text-[#849DA8] mt-[3px] ${
-                message.key === "left"
-                  ? "text-left"
-                  : "text-right"
-              }`}>{message.time}</p>
+              <h2 className="text-lg font-semibold text-white">Alison Kennedy</h2>
+              <p className="text-sm text-white ">Active Now</p>
             </div>
           </div>
-        ))}
-      </div>
-
-      {/* Input Box */}
-      <div className="bg-white p-[10px] rounded-[10px] border border-[#AEAEAE]  m-3 w-[calc(100%-24px)] left-0  right-0 ">
-        <input
-          type="text"
-          placeholder="Message Alison Kennedy..."
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-          className="w-full p-3 border-none "
-        />
-       <div className="flex justify-between items-center">
-        <div className="flex gap-2">
-        <label htmlFor="" className="relative">
-          <input type="image" src="" alt="" className="file-inputs" />
-          <PhotoUpload/>
-        </label>
-        <label htmlFor="" className="relative">
-          <input type="file" name="" id=""  className="file-inputs"/>
-          <FileUpload/>
-        </label>
+          <div className="ml-auto">
+            <button className="text-white text-xl">⋮</button>
+          </div>
         </div>
-        <button
-          onClick={sendMessage}
-          className=""
-        > <MessageSend/>
-        </button>
-       </div>
+
+        {/* Messages */}
+        <div className="flex-1 bg-white overflow-y-auto overflo-custom p-4">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`flex ${message.key === "left" ? "justify-start" : "justify-end"} mb-4`}
+            >
+              <div
+                className={`max-w-[70%]  rounded-[10px] ${message.key === "left"
+                  ? ""
+                  : ""
+                  }`}
+              >
+                <p className={`p-3 rounded-[10px] ${message.key === "left"
+                  ? "bg-[#E7F8F6] shadow-md"
+                  : "bg-[#CCE9FA]  shadow-md"
+                  }`}>{message.text}</p>
+
+                <p className={`text-xs text-[#849DA8] mt-[3px] ${message.key === "left"
+                  ? "text-left"
+                  : "text-right"
+                  }`}>{message.time}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Input Box */}
+        <form>
+          <div className="bg-white p-[10px] rounded-[10px] border border-[#AEAEAE]  m-3 w-[calc(100%-24px)] left-0  right-0 ">
+            <input
+              type="text"
+              placeholder="Message Alison Kennedy..."
+              value={inputMessage}
+              onChange={(e) => onTyping(e)}
+              className="w-full p-3 border-none text-lg"
+            />
+            <div className="flex justify-between items-center">
+              <div className="flex gap-2">
+                <label htmlFor="" className="relative">
+                  <input type="image" src="" alt="" className="file-inputs" />
+                  <PhotoUpload />
+                </label>
+                <label htmlFor="" className="relative cursor-pointer">
+                  <input type="file" name="" id="" className="file-inputs cursor-pointer" />
+                  <FileUpload />
+                </label>
+              </div>
+              <button
+                onClick={sendMessage}
+              > <MessageSend />
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
     </div>
   );
 };
