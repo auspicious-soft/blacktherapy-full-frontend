01914f0c90dd0860fbe4eb7775681c03263c801e
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-12-05 10:07:30 +0530

    profile

--- src/app/customer/dashboard/care/[id]/page.tsx
+++ src/app/customer/dashboard/care/[id]/page.tsx
@@ -1,45 +1,68 @@
-'use client'
+"use client";
 import Image from "next/image";
 import React from "react";
 import profileImage from "@/assets/images/clientpic.png";
 import useSWR from "swr";
 import { GetEmployeeRecordsData } from "@/services/admin/admin-service";
 import { useRouter } from "next/navigation";
-import { useParams, useSearchParams } from 'next/navigation';
+import { useParams, useSearchParams } from "next/navigation";
 import { getEmployeeDetails } from "@/services/client/client-service";
 
 const Page = () => {
-    const params = useParams();
-    const searchParams = useSearchParams();
-    const id = params.id as string;
-    const firstName = searchParams.get("firstName");
-    const lastName = searchParams.get("lastName");
+  const params = useParams();
+  const searchParams = useSearchParams();
+  const id = params.id as string;
+  const firstName = searchParams.get("firstName");
+  const lastName = searchParams.get("lastName");
 
-  const {data, isLoading } = useSWR(`/client/therapists/employee-records/${id}`, GetEmployeeRecordsData, {revalidateOnFocus:false})
-  const {data: employee } = useSWR(`/client/therapists/${id}`, getEmployeeDetails, {revalidateOnFocus:false})
+  const { data, isLoading } = useSWR(
+    `/client/therapists/employee-records/${id}`,
+    GetEmployeeRecordsData,
+    { revalidateOnFocus: false }
+  );
+  const { data: employee } = useSWR(
+    `/client/therapists/${id}`,
+    getEmployeeDetails,
+    { revalidateOnFocus: false }
+  );
   const records = data?.data?.data[0];
-  console.log('records:', records);
-  const employeeDetails = employee?.data?.data
-  console.log('employeeDetails:', employeeDetails);
-
+  const employeeDetails = employee?.data?.data;
 
   return (
     <div>
       <div className="text-lg">
-        <div className="mb-5 ">
-          <Image src={profileImage} alt="profile" />
+        <div className="mb-5 flex gap-5 items-center bg-white p-5 rounded-xl ">
+          <Image src={profileImage} alt="profile" width={150} height={150} className="object-contain "/>
+        <div>
+        <h2 className="leading-[normal]">{firstName} {lastName} <span className="text-lg">({records?.position})</span></h2>
+        <p>{employeeDetails?.about} </p>
         </div>
-        <p> {firstName} {lastName} <span>({records?.position})</span> </p>
-        <p>Email: {records?.employeeEmail} </p>
-        <p>Weekly Hours:  {employeeDetails?.weeklyHours} </p>
-        <p>Start Time: {employeeDetails?.startTime} </p>
-        <p>End Time: {employeeDetails?.endTime } </p>
-        {employeeDetails?.currentAvailability?.map((item: any) => {
-             <div key={item}>
-              <p>Day: {item} </p>
-            </div>
-               
-})}
+        </div>
+       <h5 className="flex items-center gap-3 mb-2.5">Email: <p className="text-base">{records?.employeeEmail} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Weekly Hours: <p className="text-base">{employeeDetails?.weeklyHours} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Start Time: <p className="text-base"> {employeeDetails?.startTime} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">End Time: <p className="text-base"> {employeeDetails?.endTime} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Rate of Pay: <p className="text-base"> {employeeDetails?.rateOfPay} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Skills: <p className="text-base"> {employeeDetails?.skills} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Additional Information: <p className="text-base"> {employeeDetails?.additionalInformation} </p></h5>
+       <h5 className="flex items-center gap-3 mb-2.5">Available on: 
+      {employeeDetails?.currentAvailability?.map((item: string, index: number) => {
+          const fullDayNames: { [key: string]: string } = {
+            Mo: "Monday",
+            Tu: "Tuesday",
+            We: "Wednesday",
+            Th: "Thursday",
+            Fr: "Friday",
+            Sa: "Saturday",
+            Su: "Sunday",
+          };
+          return (
+            <p key={item} className="text-base">
+     {fullDayNames[item] || item}{index < employeeDetails.currentAvailability.length - 1 ? ',' : ''}
+   </p>
+          )
+        })}
+        </h5>
       </div>
     </div>
   );
