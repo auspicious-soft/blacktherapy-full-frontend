30a42f99e2e8cd74de9435390324d367fa0fb314
Auspicious <info@auspicioussoft.com>
2025-02-27 09:56:58 +0530

    refactor: disable exhaustive-deps eslint rule for extra-completed-fields component

--- src/components/extra-completed-fields.tsx
+++ src/components/extra-completed-fields.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react'
 import SoapNote from './clinician-notes/soap'
 import MedicalStatusExam from './clinician-notes/medical-status-exam'
